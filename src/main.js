91275847c4701520919d9a3b30d17227b01958ba
lentoo <729533020@qq.com>
2018-05-23 17:56:22 +0800

    添加了bug监控

--- src/main.js
+++ src/main.js
@@ -20,20 +20,19 @@ new Vue({
 bingBackEvent();
 /** 监听手机物理后退按键 */
 function bingBackEvent() {
-    document.addEventListener("plusready", function () {
+    document.addEventListener("plusready", function() {
         // 注册返回按键事件 
-        plus.key.addEventListener('backbutton', function () {
+        plus.key.addEventListener('backbutton', function() {
             let index = document.toIndex || 1;
             // let path = location.href.split('#')[1];
             // if (path != '/') {
             //     history.back();
             // }
-            if(index > 7 ){
+            if (index > 7) {
                 history.back();
-            }
-            else {
+            } else {
                 // 事件处理 
-                plus.nativeUI.confirm("要离开了吗？", function (event) {
+                plus.nativeUI.confirm("要离开了吗？", function(event) {
                     console.log(event.index);
                     if (event.index == 0) {
                         plus.runtime.quit();
@@ -41,5 +40,28 @@ function bingBackEvent() {
                 }, null, ["取消", "确定"]);
             }
         }, false);
+    });
+}
+function formatComponentName(vm)
+{
+  if (vm.$root === vm) return 'root';
+
+  var name = vm._isVue ? (vm.$options && vm.$options.name) || (vm.$options && vm.$options._componentTag) : vm.name;
+  return (name ? 'component <' + name + '>' : 'anonymous component') + (vm._isVue && vm.$options && vm.$options.__file ? ' at ' + (vm.$options && vm.$options.__file) : '');
+
+}
+var fundebug = require("fundebug-javascript");
+fundebug.apikey = "39d0e66c45ab2115b87a8b1a38bebf1e7eb41d265e34690ad346e366b011c34d"
+
+Vue.config.errorHandler = function(err, vm, info) {
+    var componentName = formatComponentName(vm);
+    var propsData = vm.$options && vm.$options.propsData;
+
+    fundebug.notifyError(err, {
+        metaData: {
+            componentName: componentName,
+            propsData: propsData,
+            info: info
+        }
     });
 }
